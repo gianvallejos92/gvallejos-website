d22193d82a5a5d664b9f46ff9b837e2f57eadbca
Gian Vallejos <gian.vallejos92@gmail.com>
2025-04-29 16:06:32 -0500

    post more features

--- blog/post-more.php
+++ blog/post-more.php
@@ -3,27 +3,36 @@
         <h2>Tambien te puede interesar</h2>
     </div> 
     <div class="post-more-item-wrapper">
-        <a href="#">
+        
             <div class="post-more-item">
-                <div class="post-more-info">
-                    <div class="titulo">Todo lo que deberías saber sobre Excepciones de Apex en Salesforce</div>
-                    <div class="detalles">
-                        <span>0 comments</span>
-                        <span>2 Min Read</span>
-                        <span>6 views</span>
-                    </div>
-                </div>
-
-                <div class="post-more-author">
-                    <div class="author-img">
-                        <!--img src="http://gvallejos.local/wp-content/uploads/2025/04/1597089941038.jpg" alt="Author Post Image"-->
-                    </div>
-                    <div class="author-info">
-                        <div class="author-name">
-                            Gian Vallejos
+                <a href="#">
+                    <div class="post-more-info">
+                        <h3>Todo lo que deberías saber sobre Excepciones de Apex en Salesforce</h3>
+                        <div class="detalles">
+                            <span class="separator">0 comments</span>
+                            <span class="separator">2 Min Read</span>
+                            <span>6 views</span>
                         </div>
-                        <div class="author-date">
-                            29 Dic 2020
+                    </div>
+                </a>
+                <div class="post-more-author">
+                    <div class="post-author">
+                        <a href="#">
+                            <div class="post-author-img">
+                                <img 
+                                    src="http://gvallejos.local/wp-content/uploads/2025/04/1597089941038.jpg" 
+                                    alt="Author Post Image">
+                            </div>
+                        </a>
+                        <div class="post-author-info">
+                            <a href="#">
+                                <div class="post-author-name">
+                                    Gian Vallejos
+                                </div>
+                            </a>
+                            <div class="post-author-date">
+                                29 Dic 2020
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -31,6 +40,85 @@
                 <div class="overlay"></div>
                 <img src="http://gvallejos.local/wp-content/uploads/2025/04/technology-4237426_1280.jpg" alt="recent post image" class="post-image-style">
             </div>
-        </a>
+
+            <div class="post-more-item">
+                <a href="#">
+                    <div class="post-more-info">
+                        <h3>Todo lo que deberías saber sobre Excepciones de Apex en Salesforce</h3>
+                        <div class="detalles">
+                            <span class="separator">0 comments</span>
+                            <span class="separator">2 Min Read</span>
+                            <span>6 views</span>
+                        </div>
+                    </div>
+                </a>
+                <div class="post-more-author">
+                    <div class="post-author">
+                        <a href="#">
+                            <div class="post-author-img">
+                                <img 
+                                    src="http://gvallejos.local/wp-content/uploads/2025/04/1597089941038.jpg" 
+                                    alt="Author Post Image">
+                            </div>
+                        </a>
+                        <div class="post-author-info">
+                            <a href="#">
+                                <div class="post-author-name">
+                                    Gian Vallejos
+                                </div>
+                            </a>
+                            <div class="post-author-date">
+                                29 Dic 2020
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="overlay"></div>
+                <img src="http://gvallejos.local/wp-content/uploads/2025/04/finger-3389916_1280.jpg" alt="recent post image" class="post-image-style">
+            </div>
+
+            <div class="post-more-item">
+                <a href="#">
+                    <div class="post-more-info">
+                        <h3>Todo lo que deberías saber sobre Excepciones de Apex en Salesforce</h3>
+                        <div class="detalles">
+                            <span class="separator">0 comments</span>
+                            <span class="separator">2 Min Read</span>
+                            <span>6 views</span>
+                        </div>
+                    </div>
+                </a>
+                <div class="post-more-author">
+                    <div class="post-author">
+                        <a href="#">
+                            <div class="post-author-img">
+                                <img 
+                                    src="http://gvallejos.local/wp-content/uploads/2025/04/1597089941038.jpg" 
+                                    alt="Author Post Image">
+                            </div>
+                        </a>
+                        <div class="post-author-info">
+                            <a href="#">
+                                <div class="post-author-name">
+                                    Gian Vallejos
+                                </div>
+                            </a>
+                            <div class="post-author-date">
+                                29 Dic 2020
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="overlay"></div>
+                <img src="http://gvallejos.local/wp-content/uploads/2025/04/ai-generated-8353869_1280.jpg" alt="recent post image" class="post-image-style">
+            </div>
+        
     </div>
+
+    <div class="about-cta">
+        <a href="/blog" class="btn btn-blue">View All Posts</a>
+    </div> 
+
 </div>
